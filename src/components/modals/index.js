ae54a61b0269bd26c86e47eb762e5931e2ea5741
Nikita_Poddubnyi <piddubniimikita@gmail.com>
2026-01-12 19:04:02 +0200

    some style fix

---
+++ src/components/modals/index.js
@@ -0,0 +1,6 @@
+import AdminVerification from "./AdminVerification";
+import Notification from "./Notification";
+import AppointmentModal from "./AppointmentModal";
+import ModalBuy from "./ModalBuy";
+
+export { AdminVerification, Notification, AppointmentModal, ModalBuy };
